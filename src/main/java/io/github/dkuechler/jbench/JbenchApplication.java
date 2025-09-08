22f2db993139c047e8aaa55a1fa2edcf9da8493e
dkuechler <kuechlerdaniel34@gmail.com>
2025-09-08 19:59:05 +0200

    Initial Spring-boot setup commit

---
+++ src/main/java/io/github/dkuechler/jbench/JbenchApplication.java
@@ -0,0 +1,13 @@
+package io.github.dkuechler.jbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbenchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/dkuechler/jbench/JbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dkuechler.jbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbenchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
