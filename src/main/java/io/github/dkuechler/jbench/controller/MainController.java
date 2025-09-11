a48bb83d0d2702c636a6491dfaca0a3c66b69022
dkuechler <kuechlerdaniel34@gmail.com>
2025-09-11 12:47:14 +0200

    Login functionality/ Session scopes

--- src/main/java/io/github/dkuechler/jbench/controller/MainController.java
+++ src/main/java/io/github/dkuechler/jbench/controller/MainController.java
@@ -1,18 +1,37 @@
 package io.github.dkuechler.jbench.controller;
 
+import org.apache.commons.logging.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import io.github.dkuechler.jbench.login.LoggedUserManagementService;
+
 @Controller
 public class MainController {
-    @RequestMapping("/home")
-    public String home(@RequestParam(defaultValue = "blue") String color,
-                       @RequestParam(required = false) String name, Model page) {
-        page.addAttribute("username", name);
-        page.addAttribute("color", color);
-        return "home";
+
+    private final LoggedUserManagementService userService;
+
+    public MainController(LoggedUserManagementService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/main")
+    public String home(@RequestParam(required = false) String logout, @RequestParam(required = false) String name, Model model) {
+        if (logout != null) {
+            userService.setLoggedUsername(null);
+        }
+        if (name != null) {
+            userService.setLoggedUsername(name);
+        }
+        String username = userService.getLoggedUsername();
+        if (username == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("username", username);
+        return "main";
     }
     
 }
