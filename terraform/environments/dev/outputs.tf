f02740dbe69b40334a50419fec46e6e3f89e55e7
dkuechler <kuechlerdaniel34@gmail.com>
2026-01-01 23:34:56 +0100

    feat: add minimal terraform infrastructure for networking

---
+++ terraform/environments/dev/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.networking.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.networking.public_subnet_ids
+}
