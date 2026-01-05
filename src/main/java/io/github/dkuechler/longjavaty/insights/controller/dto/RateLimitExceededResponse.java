67a23679cd1ede94ce84928fb7d1b53a0add503d
dkuechler <kuechlerdaniel34@gmail.com>
2026-01-06 00:46:20 +0100

    fix: delete duplicate DTO file

--- src/main/java/io/github/dkuechler/longjavaty/insights/controller/dto/RateLimitExceededResponse.java
+++
@@ -1,8 +0,0 @@
-package io.github.dkuechler.longjavaty.insights.controller.dto;
-
-import java.time.OffsetDateTime;
-
-public record ThrottledResponse(
-    String message,
-    OffsetDateTime retryAfter
-) {}
